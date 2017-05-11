67c0aab4729e10413c11e194609c9787e02b71a9
Avendor7 <steve.wiggins7@gmail.com>
2017-05-11 04:24:27 -0400

    height and width init

--- public/game.js
+++ public/game.js
@@ -1,4 +1,10 @@
+//initialize canvas and context
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
-canvas.height = clientheight;
-canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
+
+//set the height and width of canvas
+//width is the size of the container (boostrap container)
+canvas.style.width='100%';
+//height is the height of the full window
+canvas.height = window.innerHeight;
+
