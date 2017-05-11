0edb66adf4cee8b10a279c67d66ff1057a2c8c2a
Avendor7 <steve.wiggins7@gmail.com>
2017-05-11 06:44:05 -0400

    notes, new init code

--- public/game.js
+++ public/game.js
@@ -1,10 +1,47 @@
-//initialize canvas and context
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
+//game objects
+var paddle1;
+var paddle2;
+var ball;
+var divider;
+var player1Score;
+var player2Score;
 
-//set the height and width of canvas
-//width is the size of the container (boostrap container)
-canvas.style.width='100%';
-//height is the height of the full window
-canvas.height = window.innerHeight;
+//initialize all the things
+function init() {
+    game.start();
+}
 
+
+//main game object
+var game = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        //set width to the container width
+        this.canvas.style.width = '100%';
+        //set height to the window height
+        this.canvas.height = window.innerHeight;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(update, 20);
+
+        //input
+        window.addEventListener('keydown', function (e) {
+            e.preventDefault();
+            game.keys = (game.keys || []);
+            game.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            game.keys[e.keyCode] = (e.type == "keydown");
+        })
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    },    
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+//update loop
+function update(){
+
+}
